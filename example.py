7e2f3b61a30650d68eec64f6fe4f5dd363608244
chuckma <chuck_ma@qq.com>
2024-11-15 18:13:36 +0800

    add task_type support

--- example.py
+++ example.py
@@ -3,13 +3,9 @@ import torch
 from nunchaku.pipelines import flux as nunchaku_flux
 
 pipeline = nunchaku_flux.from_pretrained(
-    "black-forest-labs/FLUX.1-dev",
+    "black-forest-labs/FLUX.1-schnell",
     torch_dtype=torch.bfloat16,
-    qmodel_path="mit-han-lab/svdquant-models/svdq-int4-flux.1-dev.safetensors",  # download from Huggingface
+    qmodel_path="mit-han-lab/svdquant-models/svdq-int4-flux.1-schnell.safetensors",  # download from Huggingface
 ).to("cuda")
-image = pipeline(
-    "A cat holding a sign that says hello world",
-    num_inference_steps=28,
-    guidance_scale=3.5,
-).images[0]
-image.save("/root/example.png")
+image = pipeline("A cat holding a sign that says hello world", num_inference_steps=4, guidance_scale=0).images[0]
+image.save("example.png")
