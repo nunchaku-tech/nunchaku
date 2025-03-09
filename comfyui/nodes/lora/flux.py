d61f87d47afd04918c3f4dd106b6046aaab50e0d
ashlay <edu-01@live.com>
2025-03-09 11:22:17 -0300

    Add node "SVDQuant FLUX.1 LoRA Loader (Simple)" removed conversion process load already converted loras.

--- comfyui/nodes/lora/flux.py
+++ comfyui/nodes/lora/flux.py
@@ -119,3 +119,66 @@ class SVDQuantFluxLoraLoader:
             self.cur_lora_name = lora_name
 
         return (model,)
+
+class SVDQuantFluxLoraLoaderSimple:
+    def __init__(self):
+        self.cur_lora_name = "None"
+
+    @classmethod
+    def INPUT_TYPES(s):
+        lora_name_list = [
+            "None",
+            *folder_paths.get_filename_list("loras"),
+        ]
+
+        return {
+            "required": {
+                "model": ("MODEL", {"tooltip": "The diffusion model the LoRA will be applied to."}),
+                "lora_name": (lora_name_list, {"tooltip": "The name of the LoRA."}),
+                "lora_strength": (
+                    "FLOAT",
+                    {
+                        "default": 1.0,
+                        "min": -100.0,
+                        "max": 100.0,
+                        "step": 0.01,
+                        "tooltip": "How strongly to modify the diffusion model. This value can be negative.",
+                    },
+                ),
+            }
+        }
+
+    RETURN_TYPES = ("MODEL",)
+    OUTPUT_TOOLTIPS = ("The modified diffusion model.",)
+    FUNCTION = "load_lora"
+    TITLE = "SVDQuant FLUX.1 LoRA Loader (Simple)"
+
+    CATEGORY = "SVDQuant"
+    DESCRIPTION = (
+        "Use this node to load only converted LoRA's!\r"
+        "LoRAs are used to modify the diffusion model, "
+        "altering the way in which latents are denoised such as applying styles. "
+        "Currently, only one LoRA node can be applied."
+    )
+
+    def load_lora(self, model, lora_name: str, lora_strength: float):
+        if self.cur_lora_name == lora_name:
+            if self.cur_lora_name == "None":
+                pass  # Do nothing since the lora is None
+            else:
+                model.model.diffusion_model.model.set_lora_strength(lora_strength)
+        else:
+            if lora_name == "None":
+                model.model.diffusion_model.model.set_lora_strength(0)
+            else:
+                try:
+                    lora_path = folder_paths.get_full_path_or_raise("loras", lora_name)
+                except FileNotFoundError:
+                    lora_path = lora_name
+                
+                model.model.diffusion_model.model.update_lora_params(lora_path)
+                model.model.diffusion_model.model.set_lora_strength(lora_strength)
+            
+            self.cur_lora_name = lora_name
+
+        return (model,)
