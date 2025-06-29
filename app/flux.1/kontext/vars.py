8131b5f983e640ec3f15abf944671a88be8f3b2f
Muyang Li <lmxyy1999@foxmail.com>
2025-06-29 23:59:27 +0000

    update docs

--- app/flux.1/kontext/vars.py
+++ app/flux.1/kontext/vars.py
@@ -2,13 +2,6 @@ MAX_SEED = 1000000000
 
 EXAMPLES = [
     [
-        "https://huggingface.co/mit-han-lab/svdq-int4-flux.1-canny-dev/resolve/main/logo_example.png",
-        "Convert the logo pixel style.",
-        28,
-        2.5,
-        2,
-    ],
-    [
         "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/yarn-art-pikachu.png",
         "Make Pikachu hold a sign that says 'Nunchaku is awesome', yarn art style, detailed, vibrant colors",
         28,
