2c0719cf7c6e53be952e72da5b9590709746c43a
Muyang Li <lmxyy1999@foxmail.com>
2025-06-30 08:19:33 +0000

    change the example step to 20

--- app/flux.1/kontext/vars.py
+++ app/flux.1/kontext/vars.py
@@ -4,7 +4,7 @@ EXAMPLES = [
     [
         "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/yarn-art-pikachu.png",
         "Make Pikachu hold a sign that says 'Nunchaku is awesome', yarn art style, detailed, vibrant colors",
-        28,
+        20,
         2.5,
         3,
     ],
