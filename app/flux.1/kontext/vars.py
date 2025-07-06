d6421b455a5da2551b0d6adc91a0eafc39caff94
Muyang Li <lmxyy1999@foxmail.com>
2025-07-06 18:18:32 +0000

    add more examples

--- app/flux.1/kontext/vars.py
+++ app/flux.1/kontext/vars.py
@@ -2,10 +2,41 @@ MAX_SEED = 1000000000
 
 EXAMPLES = [
     [
+        "https://huggingface.co/mit-han-lab/nunchaku-artifacts/resolve/main/ComfyUI-nunchaku/test_data/monalisa.jpg",
+        "Convert the image to a realistic style and the girl is holding a sign that says 'Nunchaku is awesome'",
+        20,
+        2.5,
+        0,
+    ],
+    [
+        "https://huggingface.co/mit-han-lab/nunchaku-artifacts/resolve/main/ComfyUI-nunchaku/test_data/mushroom_depth.webp",
+        "Transform the depth map into an ethereal fantasy image. A glowing mushroom forms the roof of an ancient treehouse, "
+        "its golden light casting warmth over moss, tiny flowers, and a stone path. Smaller glowing mushrooms create a "
+        "multi-level home among the tree’s twisted branches. In the background, a misty forest with waterfalls and a "
+        "starry night sky adds to the magical atmosphere.",
+        20,
+        2.5,
+        1,
+    ],
+    [
+        "https://huggingface.co/mit-han-lab/nunchaku-artifacts/resolve/main/ComfyUI-nunchaku/test_data/strawberry.png",
+        "Change the strawberries in the basket to a cute cat.",
+        20,
+        2.5,
+        2,
+    ],
+    [
+        "https://huggingface.co/mit-han-lab/nunchaku-artifacts/resolve/main/ComfyUI-nunchaku/test_data/logo.png",
+        "Change the logo of 'MIT HAN Lab' to 'MIT Nunchaku' in the same style.",
+        20,
+        2.5,
+        3,
+    ],
+    [
         "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/yarn-art-pikachu.png",
         "Make Pikachu hold a sign that says 'Nunchaku is awesome', yarn art style, detailed, vibrant colors",
         20,
         2.5,
-        3,
+        4,
     ],
 ]
