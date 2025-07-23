ac6c719dbc45740f874cc14fbcb350c8a425ab3d
Muyang Li <lmxyy1999@foxmail.com>
2025-07-24 06:28:40 +0800

    update

--- tests/flux/test_flux_dev_IPA.py
+++ tests/flux/test_flux_dev_IPA.py
@@ -9,12 +9,15 @@ from nunchaku import NunchakuFluxTransformer2dModel
 from nunchaku.models.IP_adapter.diffusers_adapters import apply_IPA_on_pipe
 from nunchaku.models.IP_adapter.utils import get_puild_embed, resize_numpy_image_long
 from nunchaku.utils import get_precision, is_turing
-
+import gc
+from nunchaku.pipeline.pipeline_flux_pulid import PuLIDFluxPipeline
 
 @pytest.mark.skipif(is_turing(), reason="Skip tests due to using Turing GPUs")
 def test_flux_dev_IPA():
     precision = get_precision()  # auto-detect your precision is 'int4' or 'fp4' based on your GPU
-    transformer = NunchakuFluxTransformer2dModel.from_pretrained(f"mit-han-lab/svdq-{precision}-flux.1-dev")
+    transformer = NunchakuFluxTransformer2dModel.from_pretrained(
+        f"nunchaku-tech/nunchaku-flux.1-dev/svdq-{precision}_r32-flux.1-dev.safetensors"
+    )
 
     pipeline = FluxPipeline.from_pretrained(
         "black-forest-labs/FLUX.1-dev", transformer=transformer, torch_dtype=torch.bfloat16
@@ -25,31 +28,48 @@ def test_flux_dev_IPA():
         weight_name="ip_adapter.safetensors",
         image_encoder_pretrained_model_name_or_path="openai/clip-vit-large-patch14",
     )
-    apply_IPA_on_pipe(pipeline, ip_adapter_scale=1.0, repo_id="XLabs-AI/flux-ip-adapter-v2")
+    apply_IPA_on_pipe(pipeline, ip_adapter_scale=1.1, repo_id="XLabs-AI/flux-ip-adapter-v2")
 
-    id_image = load_image("https://github.com/ToTheBeginning/PuLID/blob/main/example_inputs/liuyifei.png?raw=true")
+    id_image = load_image(
+        "https://huggingface.co/datasets/nunchaku-tech/test-data/resolve/main/ComfyUI-nunchaku/inputs/monalisa.jpg"
+    )
 
     image = pipeline(
-        prompt="A woman holding a sign that says 'SVDQuant is fast!",
+        prompt="holding an sign saying 'SVDQuant is fast!'",
         ip_adapter_image=id_image.convert("RGB"),
         num_inference_steps=50,
-        generator=torch.Generator("cuda"),
     ).images[0]
+    image.save('./tmp.png')
+
+    del pipeline
+    del transformer
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    # use the pulid pipeline to get the id embedding
+    transformer = NunchakuFluxTransformer2dModel.from_pretrained(
+        f"nunchaku-tech/nunchaku-flux.1-dev/svdq-{precision}_r32-flux.1-dev.safetensors"
+    )
+    pipeline = PuLIDFluxPipeline.from_pretrained(
+        "black-forest-labs/FLUX.1-dev",
+        transformer=transformer,
+        torch_dtype=torch.bfloat16,
+    ).to("cuda")
 
     id_image = id_image.convert("RGB")
     id_image_numpy = np.array(id_image)
     id_image = resize_numpy_image_long(id_image_numpy, 1024)
-    id_embeddings, _ = get_puild_embed(id_image, "cuda")
+    id_embeddings, _ = pipeline.pulid_model.get_id_embedding(id_image)
 
     output_image = image.convert("RGB")
     output_image_numpy = np.array(output_image)
     output_image = resize_numpy_image_long(output_image_numpy, 1024)
-    output_id_embeddings, _ = get_puild_embed(output_image, "cuda")
+    output_id_embeddings, _ = pipeline.pulid_model.get_id_embedding(output_image)
     cosine_similarities = (
         F.cosine_similarity(id_embeddings.view(32, 2048), output_id_embeddings.view(32, 2048), dim=1).mean().item()
     )
     print(cosine_similarities)
-    assert cosine_similarities > 0.70
+    assert cosine_similarities > 0.8
 
 
 if __name__ == "__main__":
