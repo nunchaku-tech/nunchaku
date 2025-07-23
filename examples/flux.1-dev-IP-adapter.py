62763ea5ad29c8f5c2c3162521fc99e57667c9db
Muyang Li <lmxyy1999@foxmail.com>
2025-07-24 05:06:59 +0800

    style: make linter happy

--- examples/flux.1-dev-IP-adapter.py
+++ examples/flux.1-dev-IP-adapter.py
@@ -35,9 +35,7 @@ IP_image = load_image(
 )
 
 image = pipeline(
-    prompt="holding an iconic starbucks cup of coffee",
-    ip_adapter_image=IP_image.convert("RGB"),
-    num_inference_steps=50
+    prompt="holding an iconic starbucks cup of coffee", ip_adapter_image=IP_image.convert("RGB"), num_inference_steps=50
 ).images[0]
 
 image.save(f"flux.1-dev-IP-adapter-{precision}.png")
