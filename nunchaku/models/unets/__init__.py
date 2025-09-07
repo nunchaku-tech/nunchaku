9a5ca4e37c6253bdb56d20d682da8ff7843ef906
dengdong <ddwinterdd@gmail.com>
2025-09-07 18:56:08 +0800

    feat: sdxl model support

---
+++ nunchaku/models/unets/__init__.py
@@ -0,0 +1,10 @@
+from .unet_sdxl import NunchakuSDXLAttention, NunchakuSDXLTransformerBlock, NunchakuSDXLShiftedConv2d, NunchakuSDXLConcatShiftedConv2d, NunchakuSDXLUNet2DConditionModel
+
+__all__ = [
+    "NunchakuSDXLAttention",
+    "NunchakuSDXLTransformerBlock",
+    "NunchakuSDXLShiftedConv2d",
+    "NunchakuSDXLConcatShiftedConv2d",
+    "NunchakuSDXLUNet2DConditionModel",
+    "NunchakuSDXLFeedForward"
+]
