5948a5028bd26ea173b9d02b7e1c3751ca8eaa6e
Muyang Li <lmxyy1999@foxmail.com>
2025-09-15 00:09:54 -0400

    make linter happy

--- examples/sdxl-turbo.py
+++ examples/sdxl-turbo.py
@@ -1,18 +1,19 @@
 import torch
 from diffusers import AutoPipelineForText2Image
+
 from nunchaku.models.unets.unet_sdxl import NunchakuSDXLUNet2DConditionModel
 
-
 if __name__ == "__main__":
-    
+
     # TODO figure out how to initialize an original SDXL pipeline without loading everything from huggingface hub.
-    pipeline = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.bfloat16, variant="fp16")
+    pipeline = AutoPipelineForText2Image.from_pretrained(
+        "stabilityai/sdxl-turbo", torch_dtype=torch.bfloat16, variant="fp16"
+    )
     prompt = "A cinematic shot of a baby racoon wearing an intricate italian priest robe."
     quantized_unet = NunchakuSDXLUNet2DConditionModel.from_pretrained("/path/to/sdxl-turbo-quantized.safetensors")
     pipeline.unet = quantized_unet
     pipeline = pipeline.to("cuda")
-    
+
     image = pipeline(prompt=prompt, guidance_scale=0.0, num_inference_steps=4).images[0]
 
     image.save("racoon.png")
-    
