cf6e355e9dd4f452b6a3558b69a7786f8ba9646f
Muyang Li <lmxyy1999@foxmail.com>
2025-09-28 20:34:34 -0400

    flux schnell test runnable

--- tests/v1/flux/test_flux_schnell.py
+++ tests/v1/flux/test_flux_schnell.py
@@ -1,13 +1,16 @@
+import gc
 import os
 from pathlib import Path
 
 import torch
 from diffusers import FluxPipeline
 
+from nunchaku import NunchakuFluxTransformer2DModelV2
 from nunchaku.utils import get_gpu_memory, get_precision, is_turing
 
-from ...utils import already_generate
+from ...utils import already_generate, compute_lpips
 from ..utils import run_pipeline
+import pytest
 
 precision = get_precision()
 torch_dtype = torch.float16 if is_turing() else torch.bfloat16
@@ -22,6 +25,7 @@ class Case:
         width: int = 1024,
         height: int = 1024,
         num_inference_steps: int = 4,
+        attention_impl: str = "flashattn2",
         expected_lpips: dict[str, float] = {},
         model_name: str = "flux.1-schnell",
         repo_id: str = "black-forest-labs/FLUX.1-schnell",
@@ -31,6 +35,7 @@ class Case:
         self.width = width
         self.height = height
         self.num_inference_steps = num_inference_steps
+        self.attention_impl = attention_impl
         self.expected_lpips = expected_lpips
         self.model_name = model_name
         self.repo_id = repo_id
@@ -49,16 +54,20 @@ class Case:
             / f"{folder_name}-bs{batch_size}"
         )
 
+        self.forward_kwargs = {
+            "width": width,
+            "height": height,
+            "num_inference_steps": num_inference_steps,
+        }
 
+
+@pytest.mark.parametrize(
+    "case", [pytest.param(Case(expected_lpips={"int4-bf16": 0.3, "fp4-bf16": 0.3}), id="flux.1-schnell-r32")]
+)
 def test_flux_schnell(case: Case):
     batch_size = case.batch_size
-    width = case.width
-    height = case.height
-    num_inference_steps = case.num_inference_steps
     expected_lpips = case.expected_lpips
     rank = case.rank
-    expected_lpips = case.expected_lpips[f"{precision}-{dtype_str}"]
-    model_name = case.model_name
     repo_id = case.repo_id
 
     dataset = [
@@ -91,9 +100,35 @@ def test_flux_schnell(case: Case):
             batch_size=case.batch_size,
             pipeline=pipeline,
             save_dir=case.save_dir_16bit,
-            forward_kwargs={
-                "height": case.height,
-                "width": case.width,
-                "num_inference_steps": case.num_inference_steps,
-            },
+            forward_kwargs=case.forward_kwargs,
         )
+
+    transformer = NunchakuFluxTransformer2DModelV2.from_pretrained(
+        f"nunchaku-tech/nunchaku-flux.1-schnell/svdq-{precision}_r{rank}-flux.1-schnell.safetensors",
+        torch_dtype=torch_dtype,
+    )
+
+    pipe = FluxPipeline.from_pretrained(repo_id, transformer=transformer, torch_dtype=torch_dtype)
+
+    if get_gpu_memory() > 18:
+        pipe.enable_model_cpu_offload()
+    else:
+        transformer.set_offload(True, use_pin_memory=True, num_blocks_on_gpu=20)
+        pipe._exclude_from_cpu_offload.append("transformer")
+        pipe.enable_sequential_cpu_offload()
+
+    run_pipeline(
+        dataset=dataset,
+        batch_size=batch_size,
+        pipeline=pipe,
+        save_dir=case.save_dir_nunchaku,
+        forward_kwargs=case.forward_kwargs,
+    )
+    del transformer
+    del pipe
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    lpips = compute_lpips(case.save_dir_16bit, case.save_dir_nunchaku)
+    print(f"lpips: {lpips}")
+    assert lpips < expected_lpips[f"{precision}-{dtype_str}"] * 1.10
