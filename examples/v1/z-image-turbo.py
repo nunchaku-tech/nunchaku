a054f75932d4ae3976287b6ec7de2448ffcbf5e6
Muyang Li <lmxyy1999@foxmail.com>
2025-12-07 13:31:24 -0500

    update example

--- examples/v1/z-image-turbo.py
+++ examples/v1/z-image-turbo.py
@@ -7,14 +7,14 @@ from nunchaku.utils import get_precision
 if __name__ == "__main__":
     precision = get_precision()  # auto-detect your precision is 'int4' or 'fp4' based on your GPU
     transformer = NunchakuZImageTransformer2DModel.from_pretrained(
-        f"/PATH/TO/svdq-{precision}_r128-z-image-turbo.safetensors"
+        f"nunchaku-tech/nunchaku-z-image-turbo/svdq-{precision}_r128-z-image-turbo.safetensors"
     )
 
     pipe = ZImagePipeline.from_pretrained(
         "Tongyi-MAI/Z-Image-Turbo",
         transformer=transformer,
         torch_dtype=torch.bfloat16,
-        low_cpu_mem_usage=False,
+        low_cpu_mem_usage=False
     ).to("cuda")
 
     prompt = "a young military male cooking in the kitchen for therapy"
@@ -23,9 +23,9 @@ if __name__ == "__main__":
         prompt=prompt,
         height=1024,
         width=1024,
-        num_inference_steps=9,  # This actually results in 8 DiT forwards
+        num_inference_steps=8,  # This actually results in 8 DiT forwards
         guidance_scale=0.0,  # Guidance should be 0 for the Turbo models
-        generator=torch.Generator("cuda").manual_seed(12345),
+        generator=torch.Generator().manual_seed(12345),
     ).images[0]
 
-    image.save(f"tmp_imgs/z-image-turbo-{precision}.png")
+    image.save(f"z-image-turbo-{precision}.png")
